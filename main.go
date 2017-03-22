814ebbe67657abd297a3d7d39c372eaa869fb7ac
Gordon Rehling <gordon.rehling@sainsburys.co.uk>
2017-03-22 15:08:47 +0000

    user story

---
+++ main.go
@@ -0,0 +1,11 @@
+package web_scrape_go
+
+// USER STORY
+//
+// As a software developer
+// I want to consume product item data from a web page and recompose it in JSON
+// So that it can be more easily re-purposed
+
+func main() {
+
+}
