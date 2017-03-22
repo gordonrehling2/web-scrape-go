91b535ac11b833ce61942344cd93cfbc9949f167
Gordon Rehling <gordon.rehling@sainsburys.co.uk>
2017-03-22 15:54:09 +0000

    implement GetWebPage with test

---
+++ getwebpage.go
@@ -0,0 +1,24 @@
+package web_scrape_go
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func GetWebPage(url string) (page []byte, err error) {
+	// Get page
+	response, err := http.Get(url)
+	if err != nil {
+		// http.Get error pass upwards
+		return
+	}
+	defer response.Body.Close()
+
+	page, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		// ioutil.ReadAll error pass upwards
+		return
+	}
+
+	return
+}
---
+++ getwebpage_test.go
@@ -0,0 +1,33 @@
+package web_scrape_go
+
+import (
+	"testing"
+)
+
+func TestGetWebPage(t *testing.T) {
+	tests := [...]struct {
+		url         string
+		wantPageLen int
+		wantErr     error
+	}{
+		// TestNo  TestData
+		0: {
+			url:         "http://geo.groupkt.com/ip/172.217.3.14/json",
+			wantPageLen: 371,
+			wantErr:     nil,
+		},
+	}
+
+	for i, td := range tests {
+		page, err := GetWebPage(td.url)
+
+		if td.wantErr != err {
+			t.Errorf("Test %d failed, expected '%s', got '%s'", i, td.wantErr, err)
+		}
+
+		if td.wantPageLen != len(page) {
+			t.Errorf("Test %d failed, expected '%d', got '%d'", i, td.wantPageLen, len(page))
+		}
+	}
+
+}
