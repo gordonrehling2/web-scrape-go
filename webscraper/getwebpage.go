d34ca85afe94b96f50e92bacfe24430276d18f5a
Gordon Rehling <gordon.rehling@sainsburys.co.uk>
2017-03-28 16:09:51 +0100

    modifications to reflect GOL update on 27-Mar

--- webscraper/getwebpage.go
+++ webscraper/getwebpage.go
@@ -5,13 +5,21 @@ import (
 	"net/http"
 )
 
-func GetWebPage(url string) (page []byte, err error) {
+// GetWebPage gets the page for the given url, with a valid cookie
+func GetWebPage(url, cookie string) (page []byte, err error) {
 	// Get page
-	response, err := http.Get(url)
+	//response, err := http.Get(url)
+	client := &http.Client{}
+	request, err := http.NewRequest("GET", url, nil)
+
+	// When GOL updated on Mon 27-Mar-17, link changed and need to add this cookie Header to get data back
+	request.Header.Set("Cookie", cookie)
+	response, err := client.Do(request)
 	if err != nil {
 		// http.Get error pass upwards
 		return
 	}
+
 	defer response.Body.Close()
 
 	page, err = ioutil.ReadAll(response.Body)
